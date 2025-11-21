330a8fca070d88eb87440c7d99defa64355faf8c
Torof <scottpanam@protonmail.com>
2025-11-21 08:15:00 +0100

    Add Alchemy API configuration

---
+++ frontend/src/config/alchemy.ts
@@ -0,0 +1,4 @@
+// Alchemy API Configuration
+export const ALCHEMY_API_KEY = "AXPXf6wiE1J549TgAUQ07";
+export const ALCHEMY_SEPOLIA_URL = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+export const ALCHEMY_NFT_API_URL = `https://eth-sepolia.g.alchemy.com/nft/v3/${ALCHEMY_API_KEY}`;
